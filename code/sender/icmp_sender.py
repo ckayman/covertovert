621e19668c0f2655cbe0d405c7fe068394ee6991
ckayman <54280042+ckayman@users.noreply.github.com>
2024-11-05 23:01:23 +0300

    Update icmp_sender.py

--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,3 +1,6 @@
-import scapy
+from scapy.all import IP, ICMP, send
 
 # Implement your ICMP sender here
+
+packet = IP(dst="receiver", ttl=1) / ICMP() # actually used the IP address from "ifconfig" output first time, but then thought the IP might change
+send(packet)
