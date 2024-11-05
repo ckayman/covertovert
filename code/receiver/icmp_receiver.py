039ecc6493f42dd8034e6e449a286808518a824d
ckayman <54280042+ckayman@users.noreply.github.com>
2024-11-05 23:02:55 +0300

    Update icmp_receiver.py

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,3 +1,16 @@
-import scapy
+from scapy.all import IP, ICMP, sniff
+import sys
 
 # Implement your ICMP receiver here
+
+def pick(packet):
+    if packet.haslayer(ICMP) and packet[IP].ttl == 1: # receiving ICMP packet whose TTL value is 1 
+        packet.show()
+        sys.exit(0)
+
+sniff(filter="icmp", prn=pick) 
+# tried using count=1 here, but it still terminates after 
+# an "incorrect" packet (e.g. a ttl=2), so a stop filter is added
+# edit: stop_filter made it printing the packet twice, so
+# imported sys to simply exit, saw no restrictions about it 
+# (in the discussion forum it is stated that it should terminate)
